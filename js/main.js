e7d7ccb9627eb3b5f7ba65919ae3ae769a507830
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2023-07-14 15:02:52 +0530

    ij

--- js/main.js
+++ js/main.js
@@ -22,7 +22,79 @@ $(document).ready(
               jQuery(this).attr('src',jQuery(this).attr('data-src'));
             })
             .fadeIn(400);
-        }
+        } 
+        $('[data-fancybox="portfolio1"]').fancybox({
+            thumbs : {
+                autoStart : true,
+                axis      : 'x'
+              },
+              helpers : { 
+                title : { type : 'inside' }
+               }
+        });
+        $('[data-fancybox="portfolio2"]').fancybox({
+            thumbs : {
+                type: "modern",
+                autoStart : true,
+                axis      : 'x'
+              }
+        });
+        $('[data-fancybox="portfolio3"]').fancybox({
+            thumbs : {
+                autoStart : true,
+                axis      : 'x'
+              }
+        });
+        $('[data-fancybox="portfolio4"]').fancybox({
+            thumbs : {
+                autoStart : true,
+                axis      : 'x'
+              }
+        });
+        $(".Port1").on("click", () => {
+            $(".portfolio1 a:first-child").trigger("click");
+        });
+        $(".Port2").on("click", () => {
+            $(".portfolio2 a:first-child").trigger("click");
+        });
+        $(".Port3").on("click", () => {
+            $(".portfolio3 a:first-child").trigger("click");
+        });
+        $(".Port4").on("click", () => {
+            $(".portfolio4 a:first-child").trigger("click");
+        });
+        
     
     }
+    
 )
+
+$(function() {
+
+    var $window = $(window);
+
+    $(document).on('click', '.GoLink', function (e) {
+        e.preventDefault();
+        var anchor = $(this).attr('data-go');
+        $(".GoLink").removeClass("active");
+        $(this).addClass("active");
+        $('html, body').animate({
+            scrollTop: $('#'+anchor).offset().top - 80
+        }, 500);
+        $("body").toggleClass("ActivePanel");
+    });
+    $(document).on('click', '.BkNw2', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#footer').offset().top - 80
+        }, 500);
+    });
+    
+    $(document).on('click', '.MenuClick', function (e) {
+        e.preventDefault();
+        $("body").toggleClass("ActivePanel");
+    });
+
+    
+
+});
